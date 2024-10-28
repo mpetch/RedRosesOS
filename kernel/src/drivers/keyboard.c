64c7a99eed31c371477792405c8f18023b83f6be
Michael Petch <mpetch@gmail.com>
2024-10-27 18:52:52 -0600

    Minor cleanup

--- kernel/src/drivers/keyboard.c
+++ kernel/src/drivers/keyboard.c
@@ -8,15 +8,12 @@ void keyboard_init() {
     IRQ_installHandler(1, keyboard_handler);
 }
 
-// the data port for the PS/2 keyboard
-#define KBD_DATA_PORT 	0x60
-
 // THE DRIVER PART WITH HANDLER
 InterruptRegisters* keyboard_handler(InterruptRegisters* regs) {
 	uint8_t scancode = inb(KBD_DATA_PORT);
 
-    // Put the scancode in the scan code buffer if it isn't full.
-    // Throw away the scan code if full
+	// Put the scancode in the scan code buffer if it isn't full.
+	// Throw away the scan code if full
 	if (!sc_isfull())
 		sc_saveelement(scancode);
 
--- kernel/src/drivers/keyboard.h
+++ kernel/src/drivers/keyboard.h
@@ -3,6 +3,9 @@
 #pragma once
 #include <interrupts/idt.h>
 
+// the data port for the PS/2 keyboard
+#define KBD_DATA_PORT   0x60
+
 InterruptRegisters* keyboard_handler(InterruptRegisters* regs);
 void keyboard_init();
 #endif
